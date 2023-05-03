5cbc17f6247dddee5b2512f9ac04d79717883cb8
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-05-03 19:33:54 -0300

    .

--- exercicios/social-media/backend/src/server.ts
+++ exercicios/social-media/backend/src/server.ts
@@ -1,8 +1,13 @@
 import { testeFn } from './teste'
 // importa classe Fastify
 import Fastify from 'fastify'
+
 // instanciar um objeto da classe fastify
 const app = Fastify()
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const testeFuncao = testeFn()
 
@@ -17,6 +22,24 @@ app.get('/teste', () => {
 
 app.get('/funcao', () => {
     return `Valor da média: ${testeFuncao}`
+})
+
+// rota para listar os posts cadastrados no banco de dados
+app.get('/posts', async () => {
+    // await indica que a função somente continua depois que os dados vierem do BD
+    const posts = await prisma.post.findMany()
+    return posts
+})
+
+app.get('posts/title', async () => {
+    const posts = await prisma.post.findMany({
+        where: {
+            title: {
+                startsWith: "Aula"
+            }
+        }
+    })
+    return posts
 })
 
 // subir o servidor HTTP
