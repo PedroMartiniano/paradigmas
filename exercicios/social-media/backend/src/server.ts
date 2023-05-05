5d291ce1634f601184de4b13da7d20d9f40d54d7
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-05-04 21:36:30 -0300

    .

--- exercicios/social-media/backend/src/server.ts
+++ exercicios/social-media/backend/src/server.ts
@@ -77,12 +77,71 @@ app.post('/post', async (request) => {
     return newPost
 })
 
+// rota para atualizar o conteúdo de um post
+app.patch('/post/content', async (request) => {
+    //cria um obj zod para definir o esquema de dados
+    const contentBody = z.object({
+        id: z.number(),
+        content: z.string()
+    })
+    //recupera os dados do frontend
+    const { id, content } = contentBody.parse(request.body)
+    // atualiza no banco de dados
+    const postUpdated = await prisma.post.update({
+        where: {
+            id: id
+        },
+        data: {
+            content: content
+        }
 
+    })
+
+    return postUpdated
+})
+
+app.patch('/post/published', async (request) => {
+    const publishedBody = z.object({
+        id: z.number(),
+        published: z.boolean()
+    })
+
+    const { id, published } = publishedBody.parse(request.body)
+
+    const postUpdated = await prisma.post.update({
+        where: {
+            id: id
+        },
+        data: {
+            published: published
+        }
+    })
+
+    return postUpdated
+})
+
+app.delete('/post/:id', async (request) => {
+    const idParam = z.object({
+        id: z.string()
+    })
+
+    const {id} = idParam.parse(request.params)
+
+    const idNumber = Number(id)
+
+    const postDeleted = await prisma.post.delete({
+        where: {
+            id: idNumber
+        }
+    })
+
+    return postDeleted
+})
 
 // subir o servidor HTTP
 app.listen({
     port: 3333
 })
     .then(() => {
-        console.log('Http Server running')
+        console.log('Http Server running on port 3333')
     })
